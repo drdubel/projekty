83cb32580d8c72f1b247f788d2046b8eae069a96
Antoni Suszko <antek.suszko@gmail.com>
2020-03-21 18:01:05 +0100

    druga lepsza wersja\

--- wisielec/wisielec.py
+++ wisielec/wisielec.py
@@ -3,7 +3,7 @@ import random
 
 def losuj_slowo():
     return random.choice([
-        'kalarepa', 'ziemniak', 'chleb', 'drogowskaz'
+        'kalarepa', 'ziemniak', 'chleb', 'drogowskaz', 'bleble', 'bobek'
     ])
 
 def generuj_maske(slowo, podane_litery):
@@ -15,17 +15,27 @@ def generuj_maske(slowo, podane_litery):
             maska.append('_')
     return maska 
 
+def podaj_litere(stare_litery):
+    while True:
+        litera = input('Podaj literę: ')
+        if not litera:
+            print('ej, miałeś podać!')
+        elif len(litera) > 1:
+            print('nie, JEDNĄ literę')
+        elif litera in stare_litery:
+            print('OK, ale taką, której jeszcze nie nie było')
+        else:
+            return litera
 
 def wisielec():
     slowo = losuj_slowo()
     slowo_s = set(slowo)
     podane_litery = set()
     belki_szubienicy = 0
-
     while True:
         maska = generuj_maske(slowo, podane_litery)
         print(' '.join(maska))
-        litera = input('jaka litera: ')
+        litera = podaj_litere(podane_litery)
         podane_litery.add(litera)
         if litera not in slowo:
             belki_szubienicy += 1
