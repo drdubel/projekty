c3ed92ca86f82b7e608666c9ec7d64bf7b1d5f5c
Antoni Suszko <antek.suszko@gmail.com>
2020-03-21 19:05:00 +0100

    wersja graficzna w żółwiu(działająca a niedokończona)

--- wisielec/wisielec.py
+++ wisielec/wisielec.py
@@ -1,5 +1,6 @@
 import random
-
+import turtle
+t=turtle.Pen()
 
 def losuj_slowo():
     return random.choice([
@@ -27,6 +28,53 @@ def podaj_litere(stare_litery):
         else:
             return litera
 
+def grafika(belk_szub):
+    if belk_szub == 1:
+        t.fd(150)
+        t.back(75)
+        t.left(90)
+    if belk_szub == 2 or belk_szub == 3:
+        t.fd(150)
+    if belk_szub == 4:
+        t.back(75)
+        t.right(45)
+        t.fd(106.06601717798213)
+        t.right(45)
+        t.up()
+        t.back(75)
+        t.down()
+    if belk_szub == 5:
+        t.fd(200)
+    if belk_szub == 6:
+        t.right(90)
+        t.fd(75)
+    if belk_szub == 7:
+        t.right(90)
+        t.circle(25)
+    if belk_szub == 8:
+        t.up()
+        t.left(90)
+        t.fd(50)
+        t.down()
+        t.fd(100)
+    if belk_szub == 9:
+        t.right(30)
+        t.fd(40)
+        t.back(40)
+        t.left(60)
+        t.fd(40)
+        t.back(40)
+        t.right(30)
+        t.back(60)
+    if belk_szub == 10:
+        t.right(30)
+        t.fd(40)
+        t.back(40)
+        t.left(60)
+        t.fd(40)
+        t.back(40)
+        t.right(30)
+        
 def wisielec():
     slowo = losuj_slowo()
     slowo_s = set(slowo)
@@ -40,6 +88,7 @@ def wisielec():
         if litera not in slowo:
             belki_szubienicy += 1
             print('pudło') 
+            grafika(belki_szubienicy)
         if belki_szubienicy == 10:
             print('wisisz')
             break
